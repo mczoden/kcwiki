6e88e6f2477dc5998bdc26fb403fb6068919bba1
Summer Sun <summersun@md101.local>
2017-04-08 20:42:45 +0800

    检查数据合法性，增加非法值容忍度

--- kcwiki_data_to_list.lua
+++ kcwiki_data_to_list.lua
@@ -14,6 +14,11 @@ local INIT = 1
 local LV99 = 2
 
 
+function error_msg (...)
+  io.stderr:write(string.format(...))
+end
+
+
 function is_interger_number (n)
   return type(n) == 'number' and n == math.floor(n)
 end
@@ -52,11 +57,7 @@ function ship_type_id_to_string (ship_type_id)
     '补给舰'
   }
 
-  if tbl[ship_type_id] == nil then
-    error('')
-  else
-    return tbl[ship_type_id]
-  end
+  return tbl[ship_type_id]
 end
 
 
@@ -67,11 +68,7 @@ function ship_speed_id_to_string (ship_speed_id)
   tbl[5] = '低速'
   tbl[10] = '高速'
 
-  if tbl[ship_speed_id] == nil then
-    error('')
-  else
-    return tbl[ship_speed_id]
-  end
+  return tbl[ship_speed_id]
 end
 
 
@@ -84,11 +81,7 @@ function ship_range_id_to_string (ship_range_id)
   }
   tbl[0] = '无'
 
-  if tbl[ship_range_id] == nil then
-    error('')
-  else
-    return tbl[ship_range_id]
-  end
+  return tbl[ship_range_id]
 end
 
 
@@ -179,76 +172,243 @@ function check_data_validity (wiki_id, init_or_lv99)
   end
 end
 
-function data_to_list (wiki_id, init_or_lv99)
-  local ship_data = orig_data.shipDataTb[wiki_id]
-  local result = ''
-  local item_suffix = is_normal_wiki_id(wiki_id) and '' or '2'
 
-  if is_normal_wiki_id(wiki_id) then
+function get_single_interger_data (from)
+  local msg = ''
+  local v = nil
+  local is_number = true
+  local ret = ''
+
+  is_number = true
+
+  if type(from) == 'string' then
+    v = tonumber(from)
+    if v == nil then
+      is_number = false
+      msg = msg .. string.format('出现字符串：%s\n', from)
+    else
+      is_number = true
+    end
+  elseif type(from) == 'number' then
+    is_number = true
+  else -- from is not number or interger
+    is_interger_number = false
+    msg = msg .. string.format('可能是一个非法类型：%s', type(from))
+  end
+
+  if is_number then
+    v = tonumber(from)
+
+    if not is_interger_number(v) then
+      msg = msg .. string.format('出现小数：%s\n', tostring(v))
+    end
+
+    if v < 0 then
+      msg = msg .. string.format('出现负数：%s\n', tostring(v))
+    end
+  end
+
+  return msg == '', tostring(from), msg
+end
+
+
+function fillin_init_lv99_data (to, from)
+  local status1, status99 = true, true
+  local msg1, msg99 = '', '' 
+
+  status1, to[INIT], msg1 = get_single_interger_data(from[INIT])
+  status99, to[LV99], msg99 = get_single_interger_data(from[LV99])
+
+  return status1 and status99, msg1 .. msg99
+end
+
+
+function ship_data_parser (wiki_id, ship_data)
+  local ship = {}
+  local k = nil
+  local v = nil
+
+  ship.wiki_id = wiki_id
+
+  v = ship_data['中文名']
+  if type(v) ~= 'string' or string.len(v) == 0 then
+    error_msg('[%s]\n', wiki_id)
+    error_msg('非法的中文名：%s\n', tostring(v))
+    error_msg('必须是字符串\n')
+    return
+  end
+  ship.zh_name = v
+
+  v = ship_data['级别'][1]
+  if type(v) ~= 'string' or string.len(v) == 0 then
+    error_msg('[%s %s]\n', wiki_id, ship.zh_name)
+    error_msg('非法的级别名：%s\n', tostring(v))
+    error_msg('必须是字符串\n')
+    return
+  end
+  ship.class = v
+
+  v = tonumber(ship_data['级别'][2])
+  if not is_interger_number(v) or v < 0 then
+    error_msg('[%s %s]\n', wiki_id, ship.zh_name)
+    error_msg('非法的舰番号：%s\n', tostring(v))
+    error_msg('必须是非负整数\n')
+    return
+  end
+  if v > 0 then
+    ship.class = ship.class .. tostring(v) .. '号'
+  end
+
+  ship.hp = {}
+  ship.firepower = {}
+  ship.armor = {}
+  ship.torpedo = {}
+  ship.evasion = {}
+  ship.aa = {}
+  ship.asw = {}
+  ship.los = {}
+  ship.luck = {}
+  local t = {
+    ['耐久'] = {
+      to = ship.hp,
+      from = ship_data['数据']['耐久']
+    },
+    ['火力'] = {
+      to = ship.firepower,
+      from = ship_data['数据']['火力']
+    },
+    ['装甲'] = {
+      to = ship.armor,
+      from = ship_data['数据']['装甲']
+    },
+    ['雷装'] = {
+      to = ship.torpedo,
+      from = ship_data['数据']['雷装']
+    },
+    ['回避'] = {
+      to = ship.evasion,
+      from = ship_data['数据']['回避']
+    },
+    ['对空'] = {
+      to = ship.aa,
+      from = ship_data['数据']['对空']
+    },
+    ['对潜'] = {
+      to = ship.asw,
+      from = ship_data['数据']['对潜']
+    },
+    ['索敌'] = {
+      to = ship.los,
+      from = ship_data['数据']['索敌']
+    },
+    ['运'] = {
+      to = ship.luck,
+      from = ship_data['数据']['运']
+    }
+  }
+  for k, v in pairs(t) do
+    local status, msg = fillin_init_lv99_data(v.to, v.from)
+    if status == false then
+      error_msg('[%s %s]\n%s\n%s', wiki_id, ship.zh_name, k, msg)
+    end
+  end
+
+  status, ship.fuel, msg = get_single_interger_data(ship_data['消耗']['燃料'])
+  if status == false then
+    error_msg('[%s %s]\n%s\n%s', wiki_id, ship.zh_name, '燃料', msg)
+  end
+  status, ship.ammo, msg = get_single_interger_data(ship_data['消耗']['弹药'])
+  if status == false then
+    error_msg('[%s %s]\n%s\n%s', wiki_id, ship.zh_name, '弹药', msg)
+  end
+
+  local index = ship_data['舰种']
+  ship.type = ship_type_id_to_string(tonumber(index))
+  if ship.type == nil then
+    error_msg('[%s %s]\n', wiki_id, ship.zh_name)
+    error_msg('%s\n使用了非法的枚举值：%s\n', '舰种', tostring(index))
+    ship.type = ''
+  end
+
+  index = ship_data['数据']['速力']
+  ship.speed = ship_speed_id_to_string(tonumber(index))
+  if ship.speed== nil then
+    error_msg('[%s %s]\n', wiki_id, ship.zh_name)
+    error_msg('%s\n使用了非法的枚举值：%s\n', '速力', tostring(index))
+    ship.speed= ''
+  end
+
+  index = ship_data['数据']['射程']
+  ship.range = ship_range_id_to_string(tonumber(index))
+  if ship.range == nil then
+    error_msg('[%s %s]\n', wiki_id, ship.zh_name)
+    error_msg('%s\n使用了非法的枚举值：%s\n', '射程', tostring(index))
+    ship.range = ''
+  end
+
+  ship.aircraft = 0
+  local tmp = 0
+  for _, v in ipairs(ship_data['装备']['搭载']) do
+    status, tmp, msg = get_single_interger_data(v)
+    if status == false then
+      if tmp ~= '-1' then
+        error_msg('[%s %s]\n', wiki_id, ship.zh_name)
+        error_msg('搭载出现了非法值：%s\n', tostring(v))
+      end
+      tmp = '0'
+    end
+    ship.aircraft = ship.aircraft + tonumber(tmp)
+  end
+  ship.aircraft = tostring(ship.aircraft)
+
+  return ship
+end
+
+
+function data_to_list (ship, init_or_lv99)
+  local result = ''
+  local item_suffix = is_normal_wiki_id(ship.wiki_id) and '' or '2'
+
+  if is_normal_wiki_id(ship.wiki_id) then
     result = result .. '{{舰娘列表\n'
-    result = result .. string.format('\t|编号 = %s\n', wiki_id)
+    result = result .. string.format('\t|编号 = %s\n', ship.wiki_id)
   end
   result = result .. string.format('\t|名字%s = %s\n',
-                                   item_suffix, ship_data['中文名'])
-  if is_normal_wiki_id(wiki_id) then
-    result = result .. string.format('\t|级别 = %s%d号\n',
-                                     ship_data['级别'][1],
-                                     tonumber(ship_data['级别'][2]))
-    result = result .. string.format('\t|类型 = %s\n',
-                                     ship_type_id_to_string(ship_data['舰种']))
+                                   item_suffix, ship.zh_name)
+  if is_normal_wiki_id(ship.wiki_id) then
+    result = result .. string.format('\t|级别 = %s\n', ship.class)
+    result = result .. string.format('\t|类型 = %s\n', ship.type)
   end
 
-  result = result .. string.format('\t|火力%s = %d\n',
-                                   item_suffix,
-                                   ship_data['数据']['火力'][init_or_lv99])
-  result = result .. string.format('\t|雷装%s = %d\n',
-                                   item_suffix,
-                                   ship_data['数据']['雷装'][init_or_lv99])
-  result = result .. string.format('\t|对空%s = %d\n',
-                                   item_suffix,
-                                   ship_data['数据']['对空'][init_or_lv99])
-  result = result .. string.format('\t|对潜%s = %d\n',
-                                   item_suffix,
-                                   ship_data['数据']['对潜'][init_or_lv99])
-  result = result .. string.format('\t|索敌%s = %d\n',
-                                   item_suffix,
-                                   ship_data['数据']['索敌'][init_or_lv99])
-  result = result .. string.format('\t|运%s = %d\n',
-                                   item_suffix,
-                                   ship_data['数据']['运'][init_or_lv99])
-  result = result .. string.format('\t|耐久%s = %d\n',
-                                   item_suffix,
-                                   ship_data['数据']['耐久'][init_or_lv99])
-  result = result .. string.format('\t|装甲%s = %d\n',
-                                   item_suffix,
-                                   ship_data['数据']['装甲'][init_or_lv99])
-  result = result .. string.format('\t|回避%s = %d\n',
-                                   item_suffix,
-                                   ship_data['数据']['回避'][init_or_lv99])
+  result = result .. string.format('\t|火力%s = %s\n',
+                                   item_suffix, ship.firepower[init_or_lv99])
+  result = result .. string.format('\t|雷装%s = %s\n',
+                                   item_suffix, ship.torpedo[init_or_lv99])
+  result = result .. string.format('\t|对空%s = %s\n',
+                                   item_suffix, ship.aa[init_or_lv99])
+  result = result .. string.format('\t|对潜%s = %s\n',
+                                   item_suffix, ship.asw[init_or_lv99])
+  result = result .. string.format('\t|索敌%s = %s\n',
+                                   item_suffix, ship.los[init_or_lv99])
+  result = result .. string.format('\t|运%s = %s\n',
+                                   item_suffix, ship.luck[init_or_lv99])
+  result = result .. string.format('\t|耐久%s = %s\n',
+                                   item_suffix, ship.hp[init_or_lv99])
+  result = result .. string.format('\t|装甲%s = %s\n',
+                                   item_suffix, ship.armor[init_or_lv99])
+  result = result .. string.format('\t|回避%s = %s\n',
+                                   item_suffix, ship.evasion[init_or_lv99])
 
-  local ship_carrying = 0
-  for _, v in ipairs(ship_data['装备']['搭载']) do
-    if v < 0 then
-      v = 0
-    end
-    ship_carrying = ship_carrying + v
-  end
-
-  result = result .. string.format('\t|搭载%s = %d\n',
-                                   item_suffix,
-                                   ship_carrying)
+  result = result .. string.format('\t|搭载%s = %s\n',
+                                   item_suffix, ship.aircraft)
   result = result .. string.format('\t|速力%s = %s\n',
-                                   item_suffix,
-                                   ship_speed_id_to_string(ship_data['数据']['速力']))
+                                   item_suffix, ship.speed)
   result = result .. string.format('\t|射程%s = %s\n',
-                                   item_suffix,
-                                   ship_range_id_to_string(ship_data['数据']['射程']))
-  result = result .. string.format('\t|燃料%s = %d\n',
-                                   item_suffix,
-                                   ship_data['消耗']['燃料'])
-  result = result .. string.format('\t|弹药%s = %d\n',
-                                   item_suffix,
-                                   ship_data['消耗']['弹药'])
+                                   item_suffix, ship.range)
+  result = result .. string.format('\t|燃料%s = %s\n',
+                                   item_suffix, ship.fuel)
+  result = result .. string.format('\t|弹药%s = %s\n',
+                                   item_suffix, ship.ammo)
   local remark = init_or_lv99 == LV99 and 'Lv99' or ''
   result = result .. string.format('\t|备注%s = %s\n',
                                    item_suffix, remark)
@@ -262,6 +422,7 @@ function main ()
   local init_ship_list_string = ''
   local lv99_ship_list_string = ''
   local ship_data_table = orig_data.shipDataTb
+  local ship = nil
 
   for id in pairs(ship_data_table) do
     table.insert(wiki_id_list, id)
@@ -276,20 +437,12 @@ function main ()
       init_ship_list_string = string.sub(init_ship_list_string, 1, -4) .. '\n'
       lv99_ship_list_string = string.sub(lv99_ship_list_string, 1, -4) .. '\n'
     end
-    if not pcall(function ()
-        init_ship_list_string = init_ship_list_string ..
-                                data_to_list(wiki_id, INIT)
-        end) then
-      check_data_validity(wiki_id, INIT)
-      os.exit(-1)
-    end
-    if not pcall(function ()
-        lv99_ship_list_string = lv99_ship_list_string ..
-                                data_to_list(wiki_id, LV99)
-         end) then
-      check_data_validity(wiki_id, LV99)
-      os.exit(-1)
-    end
+
+    ship = ship_data_parser(wiki_id, orig_data.shipDataTb[wiki_id])
+    init_ship_list_string = init_ship_list_string ..
+                            data_to_list(ship, INIT)
+    lv99_ship_list_string = lv99_ship_list_string ..
+                            data_to_list(ship, LV99)
   end
 
   print('<!-- 如果您熟悉lua和python，可参考该脚本生成页面代码')
